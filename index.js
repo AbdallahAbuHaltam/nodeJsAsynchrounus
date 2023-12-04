6c363f344539c89da53a84b840e45b78af435365
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-04 16:15:35 +0300

    building Promise

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const fs =require('fs');
 const superagent=require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`,(err,data)=>{
-    console.log(`Breed :${data}`);
-
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then(res=>{
-        console.log(res.body.message);
-
-        fs.writeFile('dog-img.txt',res.body.message,err=>{
-            console.log('Random dog img');
+const readFilePro=file=>{
+    return new Promise((resolve,reject)=>{
+        fs.readFile(file,(err,data)=>{
+            if(err) reject('I could not found this file');
+            resolve(data);
         });
-    }).catch(err=>{
+    });
+}
+const writeFilePro=(file,data)=>{
+    return new Promise((resolve,reject)=>{
+        fs.writeFile(file,data,err=>{
+         if(err) reject('I could not found this file');
+            resolve('success');
+        });
+    });
+}
+
+readFilePro(`${__dirname}/dog.txt`)
+    .then(data=>{
+        console.log(`Breed :${data}`);
+        return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)})
+        .then(res=>{
+            console.log(res.body.message);
+            return writeFilePro('dog-img.txt',res.body.message);
+    })
+    .then(
+        console.log('Random Image of dog')
+    )
+    .catch(err=>{
         console.log(err.message);
-    }
-    );
-});
+    });
+
